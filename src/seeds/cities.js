d0b6ab0c159bb0b1ef0f5bcf5b76bd66d1e072c7
Alan Alves <alan86alves@gmail.com>
2021-09-19 16:56:57 -0300

    Test

--- src/seeds/cities.js
+++ src/seeds/cities.js
@@ -25,25 +25,25 @@ fs.createReadStream('src/seeds/csv/cities.csv')
 
   .on('end', () => {
     cities.forEach(async (city) => {
-      // const upsertCountry = await prisma.cities.upsert({
-      //   where: {
-      //     id: parseInt(city.id)
-      //   },
-      //   update: {},
-      //   create: {
-      //     id: parseInt(city.id), 
-      //     name: city.name, 
-      //     latitude: city.latitude, 
-      //     longitude: city.longitude, 
-      //     is_active: true,       
-      //     slug: slug(city.state_id+"-"+city.name),    
-      //     created_at: moment().format(), 
-      //     updated_at: moment().format(),
-      //     states: {
-      //       connect: { id: parseInt(city.state_id) }
-      //     } 
-      //   }
-      // });
+      const upsertCountry = await prisma.cities.upsert({
+        where: {
+          id: parseInt(city.id)
+        },
+        update: {},
+        create: {
+          id: parseInt(city.id), 
+          name: city.name, 
+          latitude: city.latitude, 
+          longitude: city.longitude, 
+          is_active: true,       
+          slug: slug(city.state_id+"-"+city.name),    
+          created_at: moment().format(), 
+          updated_at: moment().format(),
+          states: {
+            connect: { id: parseInt(city.state_id) }
+          } 
+        }
+      });
     });
   });
 
