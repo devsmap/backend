12aa2cc07133c613de0cda655bf23de64da748ac
Alan Alves <alan86alves@gmail.com>
2021-09-20 18:18:24 -0300

    Test

--- src/seeds/states.js
+++ src/seeds/states.js
@@ -24,10 +24,10 @@ fs.createReadStream('src/seeds/csv/states.csv')
 
   .on('end', () => {
     states.forEach(async (state) => {
-      // let uule_name = state.name + ', ' + state.country_name 
-      // let encode_uule = ("w+CAIQICI"+SPECIAL_KEY_TABLE[uule_name.length]+Buffer.from(uule_name).toString('base64')).replaceAll(/[=]/g, '')
+      let uule_name = state.name + ', ' + state.country_name 
+      let encode_uule = ("w+CAIQICI"+SPECIAL_KEY_TABLE[uule_name.length]+Buffer.from(uule_name).toString('base64')).replaceAll(/[=]/g, '')
       
-      // const country = await prisma.country.findFirst({ where: { legacy_id: parseInt(state.country_id) } });
+      const country = await prisma.country.findFirst({ where: { legacy_id: parseInt(state.country_id) } });
 
       // const createState = await prisma.state.create({ 
       //   data: {
