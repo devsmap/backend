0e3ef804005331c57e55e3f074d6ebfe32484347
Alan Alves <alan86alves@gmail.com>
2021-09-20 08:37:09 -0300

    Test

--- plopfile.js
+++
@@ -1,7 +0,0 @@
-const apiModule = require('./cli/generators/apiModule')
-const apiValidationPipes = require('./cli/generators/apiValidationPipes')
-
-module.exports = plop => {
-  plop.setGenerator(...apiModule)
-  plop.setGenerator(...apiValidationPipes)
-}
