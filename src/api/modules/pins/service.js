3b3c34f7559ed41c13ff840a4532729940044a85
Alan Alves <alan86alves@gmail.com>
2021-09-21 19:10:18 -0300

    Adicionando rota de pins default

--- src/api/modules/pins/service.js
+++ src/api/modules/pins/service.js
@@ -1,4 +1,3 @@
-import { PrismaError } from 'prisma-error-enum'
 import { prisma } from '~database/client'
 
 export const allByCategories = function(category_id) {
@@ -15,18 +14,6 @@ export const allByCategories = function(category_id) {
                                   GROUP BY jobs.category_id, cities.id`,
       parseInt(category_id)
     )
-    
-    
-    prisma.$queryRaw
 
     return jobs;
 };
-
-//   jobs = Job.select(" jobs.category_id
-//   ,cities.id as city_id
-//   ,cities.latitude
-//   ,cities.longitude
-//   ,sum(1) as total")
-// .joins(:city, :company)
-// .where(category: params[:category_id].to_i, company: {is_customer: false})
-// .group('jobs.category_id, cities.id')
