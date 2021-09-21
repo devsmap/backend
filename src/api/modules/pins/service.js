b1c9a7070526730ff7855be1c3a35cbb563f1200
Alan Alves <alan86alves@gmail.com>
2021-09-21 19:10:30 -0300

    Adicionando rota de pins default

--- src/api/modules/pins/service.js
+++ src/api/modules/pins/service.js
@@ -3,15 +3,15 @@ import { prisma } from '~database/client'
 export const allByCategories = function(category_id) {
     const jobs = prisma.$queryRawUnsafe(
       `SELECT  jobs.category_id
-                                         ,cities.id as city_id
-                                         ,cities.latitude
-                                         ,cities.longitude
-                                         ,sum(1) as total
-                                  FROM jobs
-                                  INNER JOIN cities ON cities.id = jobs.city_id
-                                  INNER JOIN companies ON companies.id = jobs.company_id
-                                  WHERE category_id = $1 AND companies.is_customer = false
-                                  GROUP BY jobs.category_id, cities.id`,
+              ,cities.id as city_id
+              ,cities.latitude
+              ,cities.longitude
+              ,sum(1) as total
+      FROM jobs
+      INNER JOIN cities ON cities.id = jobs.city_id
+      INNER JOIN companies ON companies.id = jobs.company_id
+      WHERE category_id = $1 AND companies.is_customer = false
+      GROUP BY jobs.category_id, cities.id`,
       parseInt(category_id)
     )
 
