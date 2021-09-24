ab23461e533f166d135675de2d625a90ebfef4d5
Alan Alves <alan86alves@gmail.com>
2021-09-24 18:54:13 -0300

    Configurando cors

--- src/api/setup.js
+++ src/api/setup.js
@@ -7,10 +7,10 @@ import { errors } from 'celebrate'
 import { router } from './router.js'
 
 export const applyMiddlewares = app => {
+  app.use(cors({ origin: 'https://devsmap-frontend.herokuapp.com', optionsSuccessStatus: 200 }))
   app.use(express.json())
   app.use(cookieParser())
   app.use(morgan('tiny'))
   app.use(router)
   app.use(errors())
-  app.use(cors({ origin: 'https://devsmap-frontend.herokuapp.com', optionsSuccessStatus: 200 }))
 }
